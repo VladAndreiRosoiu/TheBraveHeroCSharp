6e3f815dfdbf4bcd9270fce74e5cfe6266e0f926
Vlad Andrei Rosoiu <rosoiuandreivlad@gmail.com>
2021-02-16 21:24:00 +0200

    trying C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TheBraveHero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your name : ");
+            string myName = Console.ReadLine();
+            Console.WriteLine("Hello " + myName + "! Welcome to my first app!");
+        }
+    }
+}
